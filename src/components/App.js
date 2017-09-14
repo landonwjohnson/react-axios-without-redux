46b76fb024c2a9c48ed55d9a308cd3471e78017c
Landon <landonwjohnson@gmail.com>
2017-09-14 12:22:28 -0600

    on step 7

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import Header from './Header/Header';
 import List from './List/List';
 import Workspace from './Workspace/Workspace';
-
+import { getCustomerList, postCustomer, getCustomer } from '../customers';
 
 class App extends Component {
   constructor() {
@@ -16,7 +16,48 @@ class App extends Component {
       currentCustomer: null
     }
 
+    this.startNewCustomer = this.startNewCustomer.bind( this );
+    this.createCustomer = this.createCustomer.bind( this );
   }
+
+  startNewCustomer() {
+    this.setState({
+      creating: true,
+      initialLoad: false,
+      currentCustomer: null
+
+    })
+  }
+
+  createCustomer(customer) {
+    postCustomer(customer).then(response => {
+      getCustomerList().then(list => {
+        this.setState({
+          initialLoad:true,
+          creating: false,
+          customerList: list
+        })
+      })
+    })
+  }
+
+  selectCustomer(id){
+    getCustomer(id).then(response => {
+      this.setState({
+          currentCustomer: response,
+          initialLoad: false
+        })
+      })
+    
+
+  }
+
+  componentDidMount() {
+    getCustomerList().then(list => {
+      this.setState({customerList: list});
+    })
+  }
+
 
   render() {
     return (
@@ -27,10 +68,13 @@ class App extends Component {
             this.state.customerList ?
             <List
               customerList={this.state.customerList || []}
+              startNewCustomer={this.startNewCustomer}
+              selectCustomer={this.selectCustomer}
               />
             : null
           }
           <Workspace initialLoad={this.state.initialLoad}
+                    createCustomer={this.createCustomer}
                     currentCustomer={this.state.currentCustomer}
                     creating={this.state.creating}
                   />
--- src/components/Workspace/CreateCustomer/CreateCustomer.js
+++ src/components/Workspace/CreateCustomer/CreateCustomer.js
@@ -31,6 +31,9 @@ export default class CreateCustomer extends Component {
       log: ''
     }
 
+    this.props.createCustomer( customer );
+    
+
   }
 
   render() {
